73d50b3897a81359df5992ddee41e1e0d5bce259
Nicolás Snider <nicolas.snider@outlook.com>
2025-08-11 23:24:48 -0300

    Add Stripe integration to OrdersController
    - Introduced Stripe library for payment processing.
    - Updated OrdersController constructor for logging and webhook secret.
    - Implemented StripeWebhook method to handle webhook events with error logging.
    - Added HandlePaymentIntentSucceded method to process successful payments and update order status.
    - Created ConstructStripeEvent method for validating Stripe event signatures.
    - Updated OrderStatus enum to include PaymentMismatch for payment discrepancies.
    - Enhanced OrderSpecification with a new constructor for querying by PaymentIntentId.
    - Adjusted repository calls to use fully qualified names for clarity.

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -6,12 +6,18 @@ using Core.Interfaces;
 using Core.Specifications;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace API.Controllers;
 
 [Authorize]
-public class OrdersController(ICartService cartService, IUnitOfWork unitOfWork) : BaseApiController
+public class OrdersController(
+    ICartService cartService,
+    IUnitOfWork unitOfWork,
+    ILogger<PaymentsController> logger) : BaseApiController
 {
+    private readonly string _whSecret;
+
     [HttpPost]
     public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto createOrderDto)
     {
@@ -23,7 +29,7 @@ public class OrdersController(ICartService cartService, IUnitOfWork unitOfWork) 
         var items = new List<OrderItem>();
         foreach (var item in cart.Items)
         {
-            var productItem = await unitOfWork.Repository<Product>().GetByIdAsync(item.ProductId);
+            var productItem = await unitOfWork.Repository<Core.Entities.Product>().GetByIdAsync(item.ProductId);
 
             if (productItem == null) return BadRequest("Product not found");
 
@@ -89,5 +95,66 @@ public class OrdersController(ICartService cartService, IUnitOfWork unitOfWork) 
         var order = await unitOfWork.Repository<Order>().GetEntityWithSpec(spec);
         if (order == null) return NotFound();
         return order.ToDto();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> StripeWebhook()
+    {
+        var json = await new StreamReader(Request.Body).ReadToEndAsync();
+
+        try
+        {
+            var stripeEvent = ConstructStripeEvent(json);
+            if (stripeEvent.Data.Object is not PaymentIntent intent)
+            {
+                return BadRequest("Invalid event data");
+            }
+            await HandlePaymentIntentSucceded(intent);
+            return Ok();
+        }
+        catch (StripeException ex)
+        {
+            logger.LogError(ex, "Stripe webhook error");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Stripe webhook error");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unexpected error");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Unexpected error");
+        }
+    }
+
+    private async Task HandlePaymentIntentSucceded(PaymentIntent intent)
+    {
+        if (intent.Status == "succeeded")
+        {
+            var spec = new OrderSpecification(intent.Id, true);
+            var order = await unitOfWork.Repository<Order>().GetEntityWithSpec(spec)
+                ?? throw new Exception("Order not found");
+
+            if ((long)order.GetTotal() * 100 != intent.Amount)
+            {
+                order.Status = OrderStatus.PaymentMismatch;
+            }
+            else
+            {
+                order.Status = OrderStatus.PaymentReceived;
+            }
+            await unitOfWork.Complete();
+            //todo: signalR
+        }
+    }
+
+    private Event ConstructStripeEvent(string json)
+    {
+        try
+        {
+            return EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], _whSecret);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to construct Stripe event");
+            throw new StripeException("Invalid signature");
+        }
     }
 }
--- Core/Entities/OrderAggregate/OrderStatus.cs
+++ Core/Entities/OrderAggregate/OrderStatus.cs
@@ -3,5 +3,6 @@ public enum OrderStatus
 {
     Pending,
     PaymentReceived,
-    PaymentFailed
+    PaymentFailed,
+    PaymentMismatch
 }
