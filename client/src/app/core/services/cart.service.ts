bf37f290a06b79455b02657d90e65b0438ace9f7
Nicolás Snider <nicolas.snider@outlook.com>
2025-08-13 17:10:47 -0300

    update cart.service

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -5,49 +5,46 @@ import { Cart, CartItem } from '../../shared/models/cart';
 import { Product } from '../../shared/models/product';
 import { map } from 'rxjs';
 import { DeliveryMethod } from '../../shared/models/deliveryMethod';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   baseUrl = environment.apiUrl;
-
-  private http = inject(HttpClient);
-
+  private http = inject(HttpClient)
   cart = signal<Cart | null>(null);
-
   itemCount = computed(() => {
-    return this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0) ?? 0
-  })
-
-  selectedDelivery = signal<DeliveryMethod|null>(null);
-
+    return this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0);
+  });
+  selectedDelivery = signal<DeliveryMethod | null>(null);
   totals = computed(() => {
     const cart = this.cart();
     const delivery = this.selectedDelivery();
     if (!cart) return null;
-    const subtotal = this.cart()?.items.reduce((sum, item) => sum + item.price * item.quantity, 0) ?? 0
-    const shipping = delivery?delivery.price: 0;
-    const discount = 0.1;
+    const subtotal = cart.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    const shipping = delivery ? delivery.price : 0;
+    const discount = 0;
     return {
       subtotal,
       shipping,
-      discount,
+      discount: 0,
       total: subtotal + shipping - discount
-    }
+    };
   })
 
   getCart(id: string) {
-    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(map(cart => {
-      this.cart.set(cart);
-      return cart;
-    }))
+    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
+      map(cart => {
+        this.cart.set(cart);
+        return cart;
+      })
+    )
   }
 
   setCart(cart: Cart) {
     return this.http.post<Cart>(this.baseUrl + 'cart', cart).subscribe({
-      next: (cart) => this.cart.set(cart),
-      error: (error) => console.error('Error setting cart:', error)
-    });
+      next: cart => this.cart.set(cart)
+    })
   }
 
   addItemToCart(item: CartItem | Product, quantity = 1) {
@@ -55,72 +52,67 @@ export class CartService {
     if (this.isProduct(item)) {
       item = this.mapProductToCartItem(item);
     }
-
-    cart.items = this.addOrUpdateItem(cart.items, item, quantity)
+    cart.items = this.addOrUpdateItem(cart.items, item, quantity);
     this.setCart(cart);
   }
 
   removeItemFromCart(productId: number, quantity = 1) {
     const cart = this.cart();
     if (!cart) return;
-    const index = cart.items.findIndex(x => x.productId === productId);
+    const index = cart.items.findIndex(i => i.productId === productId);
     if (index !== -1) {
       if (cart.items[index].quantity > quantity) {
         cart.items[index].quantity -= quantity;
       } else {
         cart.items.splice(index, 1);
       }
-      
-      if(cart.items.length===0){
+      if (cart.items.length === 0) {
         this.deleteCart();
       } else {
         this.setCart(cart);
       }
     }
-
   }
+
   deleteCart() {
     this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
       next: () => {
         localStorage.removeItem('cart_id');
         this.cart.set(null);
       }
-    })
-
+    });
   }
 
-  addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
+  private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number) {
     const index = items.findIndex(i => i.productId === item.productId);
     if (index === -1) {
       item.quantity = quantity;
       items.push(item);
-      return items;
-    }
-    else {
+    } else {
       items[index].quantity += quantity;
     }
     return items;
   }
 
-  private mapProductToCartItem(item: Product): CartItem {
+  private mapProductToCartItem(product: Product): CartItem {
     return {
-      productId: item.id,
-      productName: item.name,
-      price: item.price,
-      pictureUrl: item.pictureUrl,
-      brand: item.brand,
-      type: item.type,
-      quantity: 0
-    }
+      productId: product.id,
+      productName: product.name,
+      price: product.price,
+      quantity: 0,
+      pictureUrl: product.pictureUrl,
+      brand: product.brand,
+      type: product.type
+    };
   }
 
   private isProduct(item: CartItem | Product): item is Product {
     return (item as Product).id !== undefined;
   }
 
-  private createCart(): Cart {
+  private createCart() {
     const cart = new Cart();
     localStorage.setItem('cart_id', cart.id);
-    return cart
+    return cart;
   }
 }
