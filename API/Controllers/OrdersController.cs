1350ba15014b213af2642dcbc3aa798f3fb27aac
Nicolás Snider <nicolas.snider@outlook.com>
2025-08-13 17:10:33 -0300

    update OrderController

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -6,7 +6,6 @@ using Core.Interfaces;
 using Core.Specifications;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Stripe;
 
 namespace API.Controllers;
 
@@ -97,64 +96,5 @@ public class OrdersController(
         return order.ToDto();
     }
 
-    [HttpPost]
-    public async Task<IActionResult> StripeWebhook()
-    {
-        var json = await new StreamReader(Request.Body).ReadToEndAsync();
 
-        try
-        {
-            var stripeEvent = ConstructStripeEvent(json);
-            if (stripeEvent.Data.Object is not PaymentIntent intent)
-            {
-                return BadRequest("Invalid event data");
-            }
-            await HandlePaymentIntentSucceded(intent);
-            return Ok();
-        }
-        catch (StripeException ex)
-        {
-            logger.LogError(ex, "Stripe webhook error");
-            return StatusCode(StatusCodes.Status500InternalServerError, "Stripe webhook error");
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Unexpected error");
-            return StatusCode(StatusCodes.Status500InternalServerError, "Unexpected error");
-        }
-    }
-
-    private async Task HandlePaymentIntentSucceded(PaymentIntent intent)
-    {
-        if (intent.Status == "succeeded")
-        {
-            var spec = new OrderSpecification(intent.Id, true);
-            var order = await unitOfWork.Repository<Order>().GetEntityWithSpec(spec)
-                ?? throw new Exception("Order not found");
-
-            if ((long)order.GetTotal() * 100 != intent.Amount)
-            {
-                order.Status = OrderStatus.PaymentMismatch;
-            }
-            else
-            {
-                order.Status = OrderStatus.PaymentReceived;
-            }
-            await unitOfWork.Complete();
-            //todo: signalR
-        }
-    }
-
-    private Event ConstructStripeEvent(string json)
-    {
-        try
-        {
-            return EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], _whSecret);
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Failed to construct Stripe event");
-            throw new StripeException("Invalid signature");
-        }
-    }
 }
