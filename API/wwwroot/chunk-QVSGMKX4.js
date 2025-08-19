5a3db597277036a5d8d789cec3fec2e18923afa2
Nicolás Snider <nicolas.snider@outlook.com>
2025-08-19 12:27:05 -0300

    build

---
+++ API/wwwroot/chunk-QVSGMKX4.js
@@ -0,0 +1,1 @@
+import{b as u}from"./chunk-QEUZQUCF.js";import{U as t,ic as o,n as e,s as n}from"./chunk-AKPHXCJB.js";var A=(m,a)=>{let r=t(u),c=t(o);return r.currentUser()?e(!0):r.getAuthState().pipe(n(i=>i.isAuthenticated?!0:(c.navigate(["/account/login"],{queryParams:{returnUrl:a.url}}),!1)))};export{A as a};
